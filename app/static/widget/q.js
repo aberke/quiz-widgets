ac4269118f8ac0f471545572985d2be32d925bee
Alex Berke <aberke13@us155648.local>
2014-02-19 16:27:54 -0500

    push to production

--- app/static/widget/q.js
+++ app/static/widget/q.js
@@ -5,7 +5,8 @@
 
 var domain = "http://";
 //domain += '127.0.0.1:8080';
-domain += '61ad6007.ngrok.com'; // for mobile development
+//domain += '61ad6007.ngrok.com'; // for mobile development
+domain += 'quizwidget-petri.dotcloud.com';
 
 
 
